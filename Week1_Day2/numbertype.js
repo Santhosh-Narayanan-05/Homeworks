b6a886aea55f8592cf6a41da0a11d53bf1e0a106
Santhosh-Narayanan-05 <swasan05@gmail.com>
2026-03-18 15:43:35 +0530

    Day 2 works
    Changes:
        -  File structure changed
        -  Day 2 works added

---
+++ Week1_Day2/numbertype.js
@@ -0,0 +1,14 @@
+function numbertype(num)
+{
+    if(num>0){
+    console.log(num + " number is positive");
+}
+else if(num<0){
+console.log(num + " number is negative");
+} 
+else {
+    console.log(num + " number is zero");
+    
+} 
+}
+numbertype(10)
---
+++ Week1_Day2/oddoreven.js
@@ -0,0 +1,16 @@
+function isOddOrEven(num){   
+     {
+    if(num % 2 == 0){
+        console.log(num + " is even.");
+     
+    }
+    else{
+         console.log(num + " is odd.");
+    }
+}
+}
+isOddOrEven(9)
+
+
+
+    
